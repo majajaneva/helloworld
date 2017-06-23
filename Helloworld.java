db7f8c9dd98c08f5d1048b4190212a4cb3535059
majajaneva <maja_janeva@hotmail.com>
2017-06-23 11:16:51 +0200

    Update and rename Helloworld.java to Helloworld.cpp

--- Helloworld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
-        System.exit( 0 ); //success
-    }
-}
-
-
-ТИ ГО СМЕНИВ ФАЈЛОТ! ДА ВИДАМ ДАЛИ ЌЕ ПРИМЕТИШ! :)
